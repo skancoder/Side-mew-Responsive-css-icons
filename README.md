5af2128ef4ad2bf40b9835257b1d3446c578e6c6
skancoder <59524137+skancoder@users.noreply.github.com>
2023-03-21 14:24:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Side-mew-Responsive-css-icons
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-axd6ug)
---
+++ index.js
@@ -0,0 +1,25 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const themeMap = {
+  dark: 'light',
+  light: 'solar',
+  solar: 'dark',
+};
+var tmp;
+const theme =
+  localStorage.getItem('theme') ||
+  ((tmp = Object.keys(themeMap)[0]), localStorage.setItem('theme', tmp), tmp);
+const bodyClass = document.body.classList;
+bodyClass.add(theme);
+
+function toggleTheme() {
+  const current = localStorage.getItem('theme');
+  const next = themeMap[current];
+
+  bodyClass.replace(current, next);
+  localStorage.setItem('theme', next);
+}
+
+document.getElementById('themeButton').onclick = toggleTheme;
